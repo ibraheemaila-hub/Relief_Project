ed7c9a4169adb687c67b2d8f60437c6223d808ce
ibraheemaila-hub <alyltabrahym90@gmail.com>
2026-01-31 17:02:53 +0200

    main.js

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
     { area: "خانيونس", loc: "مجمع ناصر الطبي", type: "أدوية طوارئ", status: "مزدحم" },
     { area: "دير البلح", loc: "مركز إيواء الهلال", type: "وجبات ساخنة", status: "نشط" },
     { area: "رفح", loc: "منطقة تل السلطان", type: "خيام إيواء", status: "نشط" },
-    { area: "المنطقة الوسطى", loc: "دير البلح - مركز البركة", type: "توزيع دقيق", status: "نشط" }
+    { area: "المنطقة الوسطى", loc: "دير البلح - مركز البركة", type: "توزيع دقيق", status: "مزدحم" }
 ];
 
     const tableBody = $('#distribution-body');
@@ -42,4 +42,5 @@ $(document).ready(function () {
     });
 
 });
+
 
